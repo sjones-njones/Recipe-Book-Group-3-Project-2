2124aef6716ee90d7f2cd4bd777f8bcfaf2e07e3
stephanie jones <sjones.njones@gmail.com>
2023-09-07 18:07:16 -0500

    added basic get route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const { Recipe, Ingredient, User } = require('../models');
 const withAuth = require('../utils/Auth');
 
+
+
 // GET recipes for by category
 router.get('/', async (req, res) => {
       res.render('homepage', {search});
