4fa00a6a3ae872ebd4f4ce50efe2ab97da423857
stephanie jones <sjones.njones@gmail.com>
2023-09-07 18:54:13 -0500

    added seed data to recipedata

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,13 @@
 const sequelize = require('../config/connection');
 const seedUser = require('./userData');
-
+const seedRecipe = require('./recipeData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUser();
-
+  await seedRecipe();
+  
   process.exit(0);
 };
 
