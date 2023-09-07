b50ac96326c6007aa9e605b130b3206f88aa2e5d
stephanie jones <sjones.njones@gmail.com>
2023-09-07 18:04:45 -0500

    added basic get request

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,30 +1,11 @@
 const router = require('express').Router();
+const { Recipe, Ingredient, User } = require('../models');
 const withAuth = require('../utils/Auth');
 
 // GET recipes for by category
 router.get('/', async (req, res) => {
-  try {
-    const dbGalleryData = await Gallery.findAll({
-      include: [
-        {
-          model: Painting,
-          attributes: ['filename', 'description'],
-        },
-      ],
-    });
-
-    const galleries = dbGalleryData.map((gallery) =>
-      gallery.get({ plain: true })
-    );
-    res.render('homepage', {
-      galleries,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+      res.render('homepage', {search});
+  });
 
 
 module.exports = router;
