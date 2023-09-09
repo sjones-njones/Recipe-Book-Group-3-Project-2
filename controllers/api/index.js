264926fcd13a3514c09170a7203ca94134514e99
Smooth Operator <bsaikou1990@gmail.com>
2023-09-09 17:16:39 -0400

    Added recipe-routes and synced to index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const reciperoutes = require('./recipe-routes');
 
 router.use('/users', userRoutes);
+router.use('/recipes', reciperoutes);
 
 module.exports = router;
