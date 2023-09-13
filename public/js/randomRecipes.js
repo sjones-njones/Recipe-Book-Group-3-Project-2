cd49f57827500c31aff4929e9a47b6a0622dd631
sam <sammsfs@gmail.com>
2023-09-13 18:42:04 -0400

    random recipes added to homepage

---
+++ public/js/randomRecipes.js
@@ -0,0 +1,70 @@
+const fetchUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+const addedRecipeIds = [];
+
+$(document).ready(function () {
+  // Function to fetch and add a random recipe card
+  const fetchAndAddRecipeCard = () => {
+    fetch(fetchUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the JSON data
+        console.log(data);
+
+        const recipeId = data.meals[0].idMeal;
+
+        // Check if the recipe ID is already added
+        if (!addedRecipeIds.includes(recipeId)) {
+          const apiResponse = {
+            imgSrc: data.meals[0].strMealThumb,
+            cardText: data.meals[0].strMeal,
+            recipeURL: data.meals[0].strYoutube,
+          };
+
+          const card = createCard(apiResponse);
+          const cardContainer = $(".row");
+          cardContainer.append(card);
+
+          // Add the recipe ID to the array
+          addedRecipeIds.push(recipeId);
+        }
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  };
+
+  // Fetch and add 8 random recipe cards
+  for (let i = 0; i < 8; i++) {
+    fetchAndAddRecipeCard();
+  }
+});
+
+function createCard(data) {
+  const card = $("<div>").addClass("col-md-3 mb-4");
+  const cardComponent = $("<div>").addClass("card");
+  const cardLink = $("<a>")
+    .attr("href", data.recipeURL)
+    .attr("target", "_blank");
+  const cardImage = $("<img>")
+    .addClass("card-img-top")
+    .attr("src", data.imgSrc)
+    .attr("alt", "Card Image");
+  const cardBody = $("<div>").addClass("card-body");
+
+  const cardTitle = $("<h5>").addClass("card-title").text(data.cardText);
+
+  cardLink.css("text-decoration", "none"); // Remove hyperlink styling
+  cardLink.css("color", "inherit"); // Inherit text color from parent
+
+  cardBody.append(cardTitle);
+  cardLink.append(cardImage, cardBody); // Add cardImage and cardBody to the cardLink
+  cardComponent.append(cardLink); // Add the cardLink to cardComponent
+  card.append(cardComponent);
+
+  return card;
+}
